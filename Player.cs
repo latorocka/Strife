e692b1a233bf4058446eddc6ca89dc492e3f58af
latorocka <59487144+latorocka@users.noreply.github.com>
2020-12-30 12:07:37 -0800

    Add files via upload
    initial commit

---
+++ Player.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float runSpeed = 5f;
+    [SerializeField] float jumpSpeed = 5f;
+    [SerializeField] public float dashSpeed;
+    
+
+    Rigidbody2D myRigidBody;
+    Animator myAnimator;
+   
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        
+    }
+
+    void Update()
+    {
+        
+        Run();
+        FlipSprite();
+        Jump();
+        Dash();
+    }
+
+    private void Run()
+    {
+        float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, myRigidBody.velocity.y);
+        myRigidBody.velocity = playerVelocity;
+
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+        myAnimator.SetBool("Running", playerHasHorizontalSpeed);
+    }
+
+    private void FlipSprite()
+    {
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+        if (playerHasHorizontalSpeed)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(myRigidBody.velocity.x), 1f);
+        }
+    }
+
+    private void Jump()
+    {
+        if (!myRigidBody.IsTouchingLayers(LayerMask.GetMask("Ground")))
+        {
+            return;
+        }
+
+        if (CrossPlatformInputManager.GetButtonDown("Jump"))
+        {
+            Vector2 jumpVelocityToAdd = new Vector2(0f, jumpSpeed);
+            myRigidBody.velocity += jumpVelocityToAdd;
+            myAnimator.SetBool("Jumping", true);
+        }
+    }
+
+    private void Dash()
+    {
+        if (CrossPlatformInputManager.GetButtonDown("Fire1"))
+        {
+            float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+            Vector2 playerVelocity = new Vector2(controlThrow * dashSpeed, myRigidBody.velocity.y);
+            myRigidBody.velocity = playerVelocity;
+            myAnimator.SetTrigger("IsDashing");
+
+        }
+        
+        
+        
+    }
+}
